6a848a9335e07499e9fccf59b57107277d6cf86e
c7harry <76096139+c7harry@users.noreply.github.com>
2025-06-02 23:46:41 -0700

    reset state of add item/edit item button, part 9 done

--- script.js
+++ script.js
@@ -26,6 +26,15 @@ function onAddItemSubmit(e){
         return;
     }
 
+    // Check for edit mode
+    if (isEditMode){
+        const itemToEdit = itemList.querySelector('.edit-mode');
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
+    }
+
     //Create item DOM element
     addItemToDOM(newItem);
 
@@ -147,6 +156,7 @@ function clearItems() {
 
 // Checks if there are items in the list and updates the UI accordingly
 function checkUI(){
+    itemInput.value = '';
     const items = itemList.querySelectorAll('li');
     if (items.length === 0) {
         clearBtn.style.display = 'none';
@@ -155,6 +165,11 @@ function checkUI(){
         clearBtn.style.display = 'block';
         itemFilter.style.display = 'block';
     }
+
+    formBtn.innerHTML= '<i class="fa-solid fa-plus"></i> Add Item';
+    formBtn.style.backgroundColor = '#333';
+
+    isEditMode = false;
 }
 
 // Function to filter items
